9f3e3356e461db140f69a2abe56193d78c71d2dd
PrashantHanda011 <handaprashant719@gmail.com>
2023-10-03 16:48:43 +0530

    updates with smooth scroller

--- about/about.js
+++ about/about.js
@@ -1,38 +1,63 @@
-$(document).ready(function () {
-  $('.brand-carousel').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+// script.js
+const counters = document.querySelectorAll('.counter');
+
+const options = {
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      startCounting(entry.target);
+      observer.unobserve(entry.target);
+    }
   });
+}, options);
+
+function startCounting(counter) {
+  const target = parseInt(counter.getAttribute('data-target'));
+  const plus = counter.getAttribute('data-plus') === 'true';
+  let count = 0;
+  const speed = 70000; // Duration of the counting animation in milliseconds
+  const increment = target / (speed / 1000);
+
+  function updateCounter() {
+    count += increment;
+    const roundedCount = Math.floor(count);
+
+    // Add the plus sign if necessary
+    counter.textContent = plus ? `${roundedCount}+` : roundedCount;
+
+    if (count < target) {
+      requestAnimationFrame(updateCounter);
+    } else {
+      counter.textContent = plus ? `${target}+` : target;
+    }
+  }
+
+  if (!counter.classList.contains('counting')) {
+    counter.classList.add('counting');
+    requestAnimationFrame(updateCounter);
+  }
+}
+
+counters.forEach(counter => {
+  observer.observe(counter);
 });
 
+
+//story
+// const 
+const scrollingSection = window.document.querySelector('.story-container')
+console.log(scrollingSection)
+window.addEventListener('scroll', (e) => {
+  transform(scrollingSection)
+})
+
+function transform(section) {
+  const offSetTop = section.parentElement.offsetTop;
+  const scrollSection = section.querySelector('.storyScroller')
+  let percentage = ((window.scrollY - offSetTop) / window.innerHeight * 100)
+  percentage = percentage < 0 ? 0 : percentage > 420 ? 420 : percentage
+  scrollSection.style.transform = `translate3d(${-percentage}vw,0,0)`
+}
